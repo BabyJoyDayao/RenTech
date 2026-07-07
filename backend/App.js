2a826ed497bf766c2c09f6de4f193c2d93017321
BabyJoyDayao <babyjoydayao5@gmail.com>
2026-07-07 15:57:23 +0800

    Objective 1 - Week 3 Day 1 -TransactionManagement frontend and unit testing

--- backend/App.js
+++ backend/App.js
@@ -13,9 +13,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const forecastRouter = express.Router();
-registerForecastRoute(forecastRouter);
-
 const bookingRoutes = express.Router();
 const productRoutes = express.Router();
 const loginRoutes = express.Router();
